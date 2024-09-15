2b996882f0f5364f729349001de8f77d91421634
Heather Strain <64221009+hestrain@users.noreply.github.com>
2024-09-15 17:01:42 -0400

    trying more port specificity

--- server/server.js
+++ server/server.js
@@ -4,3 +4,9 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const PORT = 3001;
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })

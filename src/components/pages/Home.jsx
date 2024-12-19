982af78f7d085aecf992d1a9e86f46a61fbe482a
Heather Strain <64221009+hestrain@users.noreply.github.com>
2024-12-18 21:55:01 -0800

    fixed typos changed bio

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -88,34 +88,19 @@ export default function Home() {
               >
                 <p>
                   Hello! I am <strong>HeatherEllen Strain</strong>{" "}
-                  <i>(she/they),</i> a queer and disabled artist & developer
-                  currently working and living in Tio'tia:ke (Montreal).
+                  <i>(she/they),</i> a production/stage manager and arts administrator currently working and living in Tio'tia:ke (Montreal).
                 </p>
                 <p>
-                  I've always loved working with technology, having taken every
-                  programming course my high school offered. Out of high school
-                  I originally ent into Theatre because I loved programming
-                  Lighting Boards. Once I as at Concordia University, I found my
-                  stride in stage management, loving both building complex
-                  detailed paperwork, and being able to lead a team through to a
-                  final polished production. I've been working in the Montreal
-                  Theatre scene for the last 10 years doing Stage Management and
-                  Production Coordination & Management.
+                  I've always loved working with technology, having taken every programming course my high school offered. Out of high school I originally went into Theatre because I loved programming Lighting Boards. Once I was at Concordia University, I found my stride in stage management, loving both building complex detailed paperwork, and being able to lead a team through to a final polished production. I've been working in the Montreal Theatre scene for the last 10 years doing Stage Management and Production Coordination & Management.
                 </p>{" "}
                 <p>
-                  I found myself missing programming, adn decided to re-pursue
-                  my original passion for technology and coding. Following this
-                  instinct, I recently earned a certificate in Full Stack Web
-                  Development from the University of Toronto Coding Boot Camp.
+                  I found myself missing programming, and decided to re-pursue my original passion for technology and coding. Following this instinct, I recently earned a certificate in Full Stack Web Development from the University of Toronto Coding Boot Camp.
                 </p>
                 <p>
-                  My strengths include explaining concepts to the
-                  non-technically-minded, building efficient systems, and
-                  breaking down big ideas into achievable goals.
+                  My strengths include explaining concepts to the non-technically-minded, building efficient systems, and breaking down big ideas into achievable goals.
                 </p>
                 <p>
-                  I am skilled at managing large scale productions, leading a
-                  team, and working under pressure.
+                  I am skilled at managing large scale productions, leading a team, and working under pressure.
                 </p>
               </div>
             </Col>
@@ -151,7 +136,7 @@ export default function Home() {
           }}
         >
           <Button
-            href="https://drive.google.com/file/d/1fwVrlMwtlkQM-cUB1L6OH9I4pxgh4DNR/view?usp=sharing"
+            href="https://drive.google.com/file/d/1ZJFVSKH9QPKfaS4IuwbIZfMDa2KNjuWj/view?usp=sharing"
             target="_blank"
             style={{
               fontFamily: "Arial, Helvetica, sans-serif",
